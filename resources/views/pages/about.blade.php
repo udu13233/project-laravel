7276efe460fa749fedf968c9890c724e76280293
Junior Udeh <junior4mfm@gmail.com>
2019-03-15 09:57:38 +0100

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This the about page</p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This the laravel application from the "Laravel from Scratch" Youtube series </p>
+@endsection
